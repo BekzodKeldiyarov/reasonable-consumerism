abc90e4320982f86c3e5c006be24915f06ae3868
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-04-24 03:02:50 +0500

    added table for showing roles on profile page

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -14,7 +14,8 @@
 
                     <div class="form-group">
                         <label for="username">Username</label>
-                        <input type="text" name="username" class="form-control" id="username" value="{{$user->username}}">
+                        <input type="text" name="username" class="form-control" id="username"
+                               value="{{$user->username}}">
                     </div>
 
                     <div class="form-group">
@@ -37,7 +38,53 @@
 
         <div class="row">
             <div class="col-md-12">
-
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Roles</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                <tr>
+                                    <th>Option</th>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Slug</th>
+                                    <th>Attach</th>
+                                    <th>Detach</th>
+                                </tr>
+                                </thead>
+                                <tfoot>
+                                <tr>
+                                    <th>Option</th>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Slug</th>
+                                    <th>Attach</th>
+                                    <th>Detach</th>
+                                </tr>
+                                </tfoot>
+                                <tbody>
+                                @foreach($roles as $role)
+                                    <tr>
+                                        <td><input type="checkbox" class="form-control-sm"></td>
+                                        <td>{{$role->id}}</td>
+                                        <td>{{$role->name}}</td>
+                                        <td>{{$role->slug}}</td>
+                                        <td>
+                                            <button class="btn btn-primary">Attach</button>
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-danger">Detach</button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
